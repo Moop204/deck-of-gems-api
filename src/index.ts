c085cb166447a5d775a636725e3887d4f496207d
Moop204 <andrew.nguyen2045@outlook.com>
2021-09-26 06:36:49 +1000

    Complete deck of gem cards

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Card, CardInitialiser, Gem } from "./card";
+import { initialiser } from "./allCards";
+
+// dotenv.config();
+// const app = express();
+
+// Uses Heroku defined port number or else 3000
+// const port = process.env.PORT || 3000;
+
+// app.get("/", (req: any, res: any) => {
+//   res.status(200).json({ base: "other" });
+// });
+
+// app.listen(port, () => console.log("server started"));
+
+// const params: CardInitialiser = {
+//   reward: Gem.BLUE,
+//   point: 2,
+//   tier: 2,
+//   blackCost: 3,
+//   whiteCost: 1,
+//   blueCost: 5,
+//   redCost: 2,
+//   greenCost: 3,
+// };
+
+// const c = new Card(params);
+
+// console.log(c.toJSON());
+
+const allCards = initialiser();
+allCards.forEach((card) => console.log(card.toJSON()));
