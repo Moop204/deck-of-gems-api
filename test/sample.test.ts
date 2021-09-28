e3b665c887f8fa60cd064580d208510b24a4e835
Moop204 <andrew.nguyen2045@outlook.com>
2021-09-28 11:35:55 +1000

    Encoding and decoding tests moved to own file

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,6 +1,4 @@
 import { Card, Tier } from "../src/Card";
-import { Deck } from "../src/Deck";
-import { decodeId } from "../src/generateId";
 
 jest.mock("../src/Card");
 
@@ -9,35 +7,4 @@ it("should mock class Card", () => {
     return 2 as Tier;
   });
   jest.spyOn(Card.prototype, "getTier").mockImplementation(functionNameMock);
-});
-
-describe("testing encoding functions", () => {
-  it("encodes an empty deck", () => {
-    const parameter: number[] = [];
-    const deck = new Deck(parameter);
-    const hash = deck.generateId();
-    const decoded = decodeId(hash);
-    expect(decoded).toEqual(parameter);
-  });
-  it("encodes full deck", () => {
-    const parameter: number[] = Array.from(Array(90).keys());
-    const deck = new Deck(parameter);
-    const hash = deck.generateId();
-    const decoded = decodeId(hash);
-    expect(decoded).toEqual(parameter);
-  });
-  it("encodes subsection of deck", () => {
-    const parameter: number[] = Array.from(Array(90).keys()).slice(7, 23);
-    const deck = new Deck(parameter);
-    const hash = deck.generateId();
-    const decoded = decodeId(hash);
-    expect(decoded).toEqual(parameter);
-  });
-  it("encodes arbitrary unordered selection of cards", () => {
-    const parameter: number[] = [7, 23, 11, 26, 1];
-    const deck = new Deck(parameter);
-    const hash = deck.generateId();
-    const decoded = decodeId(hash);
-    expect(decoded.sort()).toEqual(parameter.sort());
-  });
 });
